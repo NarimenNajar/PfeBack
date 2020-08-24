1a9303ec0b8ebf98a2b2097cde5b3d62a832e77c
NarimenNajar <narimen.najar@esprit.tn>
2020-08-24 13:50:12 +0100

    services syllabus : v1

---
+++ BackFormationPN-ejb/src/main/java/tn/nouvelair/BackFormationPN/services/SyllabusService.java
@@ -0,0 +1,53 @@
+package tn.nouvelair.BackFormationPN.services;
+import tn.nouvelair.BackFormationPN.entities.Exercice;
+import tn.nouvelair.BackFormationPN.entities.Syllabus;
+
+import java.util.logging.Logger;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+
+@Stateless
+@LocalBean
+public class SyllabusService implements SyllabusServiceRemote {
+    @PersistenceContext(unitName = "OTDAV-ejb")
+    EntityManager em ;
+
+    @Override
+    public void AjouterSyllabus(Syllabus syllabus) {
+        em.persist(syllabus);
+
+    }
+
+    @Override
+    public List<Syllabus> GetSyllabuss() {
+        List<Syllabus> syllabuss=null;
+        TypedQuery<Syllabus> query = em.createQuery("Select e from Syllabus e "
+                , Syllabus.class);
+        try {
+            syllabuss = query.getResultList();
+        } catch (NoResultException e ) {
+
+        }
+        return syllabuss;
+    }
+
+    @Override
+    public void UpdateSyllabus(Syllabus syllabus) {
+        em.merge(syllabus);
+
+    }
+
+    @Override
+    public void deleteSyllabus(int idSyllabus) {
+        em.remove(em.find(Syllabus.class, idSyllabus));
+
+    }
+}
---
+++ BackFormationPN-ejb/src/main/java/tn/nouvelair/BackFormationPN/services/SyllabusServiceRemote.java
@@ -0,0 +1,14 @@
+package tn.nouvelair.BackFormationPN.services;
+import tn.nouvelair.BackFormationPN.entities.Exercice;
+import tn.nouvelair.BackFormationPN.entities.Syllabus;
+
+import javax.ejb.Remote;
+import java.util.List;
+
+@Remote
+public interface SyllabusServiceRemote {
+    public void AjouterSyllabus(Syllabus syllabus);
+    public void UpdateSyllabus(Syllabus syllabus);
+    List<Syllabus> GetSyllabuss();
+    public void deleteSyllabus(int idSyllabus);
+}
